8e47b8c6326b7618011cf74ef33102fc53bb454d
Marcin Starzak <117392254+Jogurtonelle@users.noreply.github.com>
2023-01-08 13:27:02 +0100

    new tables and changes to 'students' table
    -nowa tabela 'parents' przechowująca dane o rodzicach uczniów
    -nowa tabela 'classrooms' przechowująca dane o salach lekcyjnych
    -nowa tabela 'classroom_roles' opisująca specialne przeznaczenia sal lekcyjnych
    -nowa tabela 'administration_employees' przechowująca dane o pracownikach administracji szkoły
    -zmiany w tabeli 'students' - teraz każdy uczeń ma PESEL i ID swoich rodziców

--- sql/Schemas.sql
+++ sql/Schemas.sql
@@ -9,22 +9,47 @@ drop table if exists lessons_groups;
 drop table if exists students;
 drop table if exists teachers;
 drop table if exists subjects;
+drop table if exists parents;
+drop table if exists classrooms;
+drop table if exists classroom_roles;
+drop table if exists administration_employees;
 
+-- Tabela przechowuje informacje o rodzicach uczniow
+CREATE TABLE parents (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name varchar(32) NOT NULL,
+    surname varchar(32) NOT NULL,
+    pesel varchar(11) NOT NULL UNIQUE,
+        CHECK (pesel LIKE '___________'), -- sprawdzenie czy PESEL ma 11 cyfr
+    adress_street varchar (128),
+    adress_city varchar (128),
+    adress_postal_code varchar(6),
+        -- CONSTRAINT good_postal CHECK (adress_postal_code LIKE '[0-9][0-9][-][0-9][0-9][0-9]'),
+    phone_number varchar(9) UNIQUE
+);
 
 create table students
 (
     id       int         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name     varchar(10) NOT NULL,
-    surname  varchar(10) NOT NULL,
+    name     varchar(32) NOT NULL,
+    surname  varchar(32) NOT NULL,
+    pesel    varchar(11) NOT NULL UNIQUE,
     sex      char        NOT NULL CHECK (sex in ('k', 'm')),
-    birthday DATE
+    birthday DATE        NOT NULL,
+    first_parent_id INT,
+    FOREIGN KEY (first_parent_id) REFERENCES parents (id),
+    second_parent_id INT,
+    FOREIGN KEY (second_parent_id) REFERENCES parents (id),
+
+    CONSTRAINT CHECK (pesel LIKE '___________') -- sprawdzenie czy PESEL ma 11 cyfr
+    -- CONSTRAINT CHECK (pesel NOT IN (SELECT parents.pesel FROM parents)) -- sprawdzenie czy dziecko nie ma takiego peselu jak ktorys rodzic
 );
 
 create table teachers
 (
     id       int         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name     varchar(10) NOT NULL,
-    surname  varchar(10) NOT NULL,
+    name     varchar(32) NOT NULL,
+    surname  varchar(32) NOT NULL,
     sex      char        NOT NULL CHECK (sex in ('k', 'm')),
     birthday DATE
 );
@@ -33,7 +58,7 @@ create table teachers
 create table subjects
 (
     id      int         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name varchar(10) NOT NULL,
+    name varchar(32) NOT NULL,
     description varchar(255)
 );
 
@@ -76,3 +101,30 @@ create table lessons_table
     CHECK ( start_time < end_time )
 
 );
+
+-- Tabela przechowuje specialne role sal lekcyjnych
+CREATE TABLE classroom_roles(
+    id INT NOT NULL PRIMARY KEY,
+    description varchar(128)
+);
+
+-- Tabela przechowuje wszelkie dane na temat sal lekcyjnych
+CREATE TABLE classrooms(
+    id INT NOT NULL PRIMARY KEY, -- jako ID traktuje numer sali ktory widnieje na drzwiach, wiec nie ma AUTO_INCREMENT
+    capacity TINYINT, -- ile uczniow miesci sie w sali
+    has_projector TINYINT, -- czy sala ma projektor multimedialny?
+    special_role TINYINT REFERENCES classroom_roles(id), -- czy sala jest jakas specialna? np. sala gimnastyczna, informatyczna, chemiczna itd.
+    last_renovation DATE -- data ostatniego remontu
+);
+
+-- Tabela przechowuje informacje o pracownikach takich jak: sprzataczka, wozny, sekretarka itd.
+CREATE TABLE administration_employees(
+    id       int         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name     varchar(32) NOT NULL,
+    surname  varchar(32) NOT NULL,
+    pesel    varchar(11) NOT NULL UNIQUE,
+    sex      char        NOT NULL CHECK (sex in ('k', 'm')),
+    birthday DATE        NOT NULL,
+    role     varchar(128),
+    salary   DECIMAL(10,2)
+)
